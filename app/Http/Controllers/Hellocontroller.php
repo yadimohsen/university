b2c3577a23a3e6433a263165b69d3c6f42098c79
yadimohsen <yadimohsen@yahoo.com>
2021-05-11 23:53:58 +0430

    ver 5.0 from github

--- app/Http/Controllers/Hellocontroller.php
+++ app/Http/Controllers/Hellocontroller.php
@@ -9,6 +9,7 @@ class Hellocontroller extends Controller
  public function sayhello(){
      return 'goodbye Mohsen in ver 3.0';
      return 'Iam very tired';
+     return 'change applyed within downloaded file from server';
      
      }  //
 
